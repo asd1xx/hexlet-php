5beb3c6bb4fb6ddaa27d54704d751e4721959671
asd2d <872122@mail.ru>
2024-07-01 12:45:35 +0300

    добавлен html код в index.html

--- index.php
+++ index.php
@@ -7,3 +7,14 @@ require_once __DIR__ . '/vendor/autoload.php';
 use Hexlet\Php\Runner;
 
 print_r(Runner\run());
+
+?>
+
+<html>
+    <head>
+        <title>PHP Test</title>
+    </head>
+    <body>
+        <?php echo '<p>Hello World</p>'; ?>
+    </body>
+</html>
